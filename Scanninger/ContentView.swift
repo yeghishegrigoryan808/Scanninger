f9d1ce8d276a6c65bd3fb0876b43832a4c771dd2
Yeghishe <yeghishe.grigoryan808@gmail.com>
2026-01-13 22:48:39 +0400

    Initial Commit

---
+++ Scanninger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scanninger
+//
+//  Created by Yeghishe Grigoryan on 13.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!????")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scanninger/ScanningerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanningerApp.swift
+//  Scanninger
+//
+//  Created by Yeghishe Grigoryan on 13.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ScanningerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
